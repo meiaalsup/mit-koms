69f71c9668e92f8440f0689abb13b2a9244ed62c
Meia <meia.alsup@gmail.com>
2020-06-19 23:30:23 -0500

    second attempt at hheroku chrome backend

--- api/QueryEngine.py
+++ api/QueryEngine.py
@@ -17,7 +17,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
-GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
 CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
 
 
@@ -33,10 +32,13 @@ class QueryEngine(object):
 
     def _initialize_webdriver(self):
         chrome_options = Options()
-        chrome_options.add_argument("--headless")
+        chrome_options.binary_location = os.environ.get(
+            'GOOGLE_CHROME_BIN',
+            "chromedriver"
+        )
         chrome_options.add_argument('--disable-gpu')
         chrome_options.add_argument('--no-sandbox')
-        chrome_options.binary_location = GOOGLE_CHROME_PATH
+        chrome_options.add_argument("--headless")
         self.driver = selenium.webdriver.Chrome(
             ChromeDriverManager()
             .install(),
