0e94c2f496ee8c30d57ed9ea30f671cb282f4c1e
Meia <meia.alsup@gmail.com>
2020-06-19 21:35:15 -0500

    update api to be single function

--- api/api.py
+++ api/api.py
@@ -3,12 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/miles')
-def miles():
-    return {'miles': 7}
-
-
-@app.route('/lee')
-def lee():
-    return {'lee': 16}
-
+@app.route('/koms')
+def koms():
+    return {'miles': 7, 'lee':16}
