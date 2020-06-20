27a569606f8d0cceb6d1a6593911729afc687ea1
jacobkahn <jacobkahn1@gmail.com>
2020-06-19 22:34:10 -0500

    smallest possible ui effort exerted

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import './App.css';
+import './App.css'
+import both from './images/both.jpeg'
+import lee_gu from './images/lee_gu.jpeg'
+import miles_bread from './images/miles_bread.jpeg'
 
 class App extends React.Component {
 
@@ -15,17 +18,17 @@ class App extends React.Component {
       method: "GET",
     }).then(response => response.json())
       .then(json => {
-        this.setState({results: json})
+        this.setState({ results: json })
         console.log(json)
       })
   }
 
-  
+
 
   render() {
     let people = Object.keys(this.state.results)
     let komCounts = people.map(
-      (name, index) => 
+      (name, index) =>
         <div key={index} className="count">
           <p>{name}'s KOMs: {this.state.results[name]}.</p>
         </div>
@@ -34,10 +37,14 @@ class App extends React.Component {
     return (
       <div className="App">
         <header className="App-header">
+          <div className="image-box">
+            <img className='some-image' src={lee_gu} />
+            <img className='some-image' src={both} />
+            <img className='some-image' src={miles_bread} />
+          </div>
           <div className="countwrapper">
             {komCounts}
           </div>
-
         </header>
       </div>
     );
