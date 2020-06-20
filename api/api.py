ec0c68f3c0170080ffdcd781c76ef1c3f34005ca
Meia <meia.alsup@gmail.com>
2020-06-19 21:23:31 -0500

    adding in front end

--- api/api.py
+++ api/api.py
@@ -2,6 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+
+@app.route('/miles')
+def miles():
+    return {'miles': 7}
+
+
+@app.route('/lee')
+def lee():
+    return {'lee': 16}
+
--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [lee] = leeCount();
+
+  leeCount(() => {
+    fetch('/lee').then(res => res.json()).then(data => {
+      return data['lee']
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <div className="count">
+          <p>Lee's KOMs: {lee}.</p>
+        </div>
       </header>
     </div>
   );
