20468eafd6f08234204d0911c2a93a5531173404
jacobkahn <jacobkahn1@gmail.com>
2020-06-22 08:35:43 -0500

    python is pain

--- app/app.py
+++ app/app.py
@@ -36,7 +36,7 @@ class QueryEngine(object):
     def _initialize_webdriver(self):
         chrome_options = Options()
 
-        if os.environ.get('FLASK_ENV') == 'PRODUCTION':
+        if os.environ.get('FLASK_ENV') == 'production':
             chrome_options.binary_location = os.environ.get(
                 'GOOGLE_CHROME_BIN',
                 "chromedriver"
@@ -46,7 +46,9 @@ class QueryEngine(object):
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
         chrome_options.add_argument("--headless")
-        if os.environ.get('FLASK_ENV') == 'PRODUCTION':
+
+
+        if os.environ.get('FLASK_ENV') == 'production':
             self.driver = selenium.webdriver.Chrome(
                 executable_path=os.environ.get("CHROMEDRIVER_PATH"),
                 options=chrome_options)
