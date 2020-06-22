01ecab6425d0508b1da84b03bec65b6f2bf6aa2b
jacobkahn <jacobkahn1@gmail.com>
2020-06-22 09:29:54 -0500

    Change some proxy and server urls

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
-let SERVER_URL = "http://localhost:3000"
+let SERVER_URL = "http://localhost:5000"
 if (process.env.NODE_ENV !== "development") {
   SERVER_URL = "https://mit-koms-backend.herokuapp.com"
 }
+
 module.exports = {
   SERVER_URL: SERVER_URL
 }
