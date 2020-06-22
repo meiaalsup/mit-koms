a61cd69aeca1683990ef23feb7d0d4b3ccfb13eb
jacobkahn <jacobkahn1@gmail.com>
2020-06-22 08:27:34 -0500

    Add some sort of thing that makes the chrome driver write to /tmp

--- app/app.py
+++ app/app.py
@@ -44,6 +44,7 @@ class QueryEngine(object):
 
         chrome_options.add_argument('--disable-gpu')
         chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
         chrome_options.add_argument("--headless")
         if os.environ.get('FLASK_ENV') == 'PRODUCTION':
             self.driver = selenium.webdriver.Chrome(
